14994e885bd6d4255098df988997a80c5348203c
Alejandro Roa <alejodev04@gmail.com>
2020-03-07 15:49:40 -0400

    config thunks

--- src/thunk.js
+++ src/thunk.js
@@ -1,6 +1,58 @@
+const FETCH_START = 'start'
+const FETCH_SUCCESS = 'success'
+const FETCH_ERROR = 'error'
+
+const fetch_start = () => ({
+    type: FETCH_START
+})
+const fetch_success = payload => ({
+    type: FETCH_SUCCESS,
+    payload
+})
+const fetch_error = error => ({
+    type: FETCH_ERROR,
+    error
+})
+
+const url = 'https://jsonplaceholder.typicode.com/users'
+
+const initialState = {
+    data: [],
+    fetching: false,
+    error: null
+}
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case FETCH_START:
+            return {
+                ...state,
+                fetching: true
+            }
+        case FETCH_SUCCESS:
+            return {
+                ...state,
+                data: action.payload
+            }
+        case FETCH_ERROR:
+            return {
+                ...state,
+                data: action.error
+            }
+        default:
+            return state
+    }
+}
+
 export default payload => {
-    return function(dispatch, getState) {
-        const state = getState()
-        console.log(state)
+    return async function (dispatch, getState) {
+        dispatch(fetch_start())
+        try {
+            const result = await fetch(url)
+            const json = await result.json()
+            dispatch(fetch_success(json))
+        } catch (error) {
+            dispatch(fetch_error(error))
+        }
     };
 }
